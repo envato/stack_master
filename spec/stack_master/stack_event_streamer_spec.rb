56c33f4c39214fa264bb0711a2fbb133d6b14783
Steve Hodgkiss <steve@hodgkiss.me>
2015-10-27 11:10:51 +1100

    Add ability to stream stack events until a finish state is reached

---
+++ spec/stack_master/stack_event_streamer_spec.rb
@@ -0,0 +1,27 @@
+RSpec.describe StackMaster::StackEventStreamer do
+  let(:events_first_call) { [
+    OpenStruct.new(event_id: '1', resource_status: 'BLAH', timestamp: Time.now),
+    OpenStruct.new(event_id: '2', resource_status: 'BLAH', timestamp: Time.now),
+    OpenStruct.new(event_id: '3', resource_status: 'BLAH', timestamp: Time.now),
+  ] }
+  let(:events_second_call) {
+    events_first_call + [
+      OpenStruct.new(event_id: '4', resource_status: 'UPDATE_COMPLETE', timestamp: Time.now)
+    ]
+  }
+  let(:stack_name) { 'stack-name' }
+  let(:region) { 'us-east-1' }
+  let(:now) { Time.now }
+
+  before do
+    allow(StackMaster::StackEventFetcher).to receive(:fetch).with(stack_name, region, from: now).and_return(events_first_call, events_second_call)
+    allow(Time).to receive(:now).and_return(now)
+  end
+
+  it 'returns after seeing a finish state' do
+    events = []
+    StackMaster::StackEventStreamer.stream(stack_name, region, sleep_between_fetches: 0) do |event|
+      events << event
+    end
+  end
+end
