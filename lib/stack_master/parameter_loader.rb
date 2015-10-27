ce4279e7083e587190d1826cf39b0ed0ad0e990a
Steve Hodgkiss <steve@hodgkiss.me>
2015-10-27 15:35:48 +1100

    Fix for empty yaml files

--- lib/stack_master/parameter_loader.rb
+++ lib/stack_master/parameter_loader.rb
@@ -3,7 +3,7 @@ module StackMaster
     def self.load(parameter_files)
       parameter_files.reduce({}) do |hash, file_name|
         parameters = if File.exists?(file_name)
-          YAML.load(File.read(file_name))
+          YAML.load(File.read(file_name)) || {}
         else
           {}
         end
--- spec/stack_master/parameter_loader_spec.rb
+++ spec/stack_master/parameter_loader_spec.rb
@@ -15,6 +15,18 @@ RSpec.describe StackMaster::ParameterLoader do
     end
   end
 
+  context "an empty stack parameter file" do
+    before do
+      allow(File).to receive(:exists?).with('/base_dir/parameters/stack_name.yml').and_return(true)
+      allow(File).to receive(:exists?).with('/base_dir/parameters/us-east-1/stack_name.yml').and_return(false)
+      allow(File).to receive(:read).with('/base_dir/parameters/stack_name.yml').and_return("")
+    end
+
+    it "returns an empty hash" do
+      expect(parameters).to eq({})
+    end
+  end
+
   context "stack parameter file" do
     before do
       allow(File).to receive(:exists?).with('/base_dir/parameters/stack_name.yml').and_return(true)
