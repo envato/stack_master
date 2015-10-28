b4c95a53593a48640e21ba51351ea12e69eb3163
Steve Hodgkiss <steve@hodgkiss.me>
2015-10-28 11:56:49 +1100

    Make stack differ work with Stack’s

--- spec/stack_master/stack_differ_spec.rb
+++ spec/stack_master/stack_differ_spec.rb
@@ -1,38 +1,33 @@
 RSpec.describe StackMaster::StackDiffer do
-  subject(:stack_master) { described_class.new(stack_definition) }
-  let(:stack_definition) do
-    StackMaster::Config::StackDefinition.new(
-      region: 'us-east-1',
-      stack_name: 'myapp_vpc',
-      template: 'myapp_vpc.json',
-      tags: { 'environment' => 'production' },
-      base_dir: File.expand_path('spec/fixtures')
-    )
-  end
-  let(:stack) { StackMaster::Stack.new(stack_name: 'myapp_vpc', region: 'us-east-1', stack_id: 123) }
+  subject(:stack_master) { described_class.new(proposed_stack, stack) }
+  let(:stack) { StackMaster::Stack.new(stack_name: stack_name,
+                                       region: region,
+                                       stack_id: 123,
+                                       template_body: '{}',
+                                       parameters: {}) }
+  let(:proposed_stack) { StackMaster::Stack.new(stack_name: stack_name,
+                                                region: region,
+                                                parameters: { 'param1' => 'hello'},
+                                                template_body: "{\"a\": 1}") }
+  let(:stack_name) { 'myapp-vpc' }
+  let(:region) { 'us-east-1' }
 
   describe "#perform" do
     context "entirely new stack" do
-      before do
-        allow(StackMaster::Stack).to receive(:find).with('us-east-1', "myapp_vpc").and_return nil
-        allow(stack_definition).to receive(:template_body).and_return("new stack body")
-      end
+      let(:stack) { nil }
 
       it "outputs the entire stack" do
-        expect { stack_master.perform }.to output("Stack diff: \n\e[0;32;49m+new stack body\n\e[0m\\ No newline at end of file\nParameters diff: \n\e[0;32;49m+{\n\e[0m\e[0;32;49m+  \"param_1\": \"hello\"\n\e[0m\e[0;32;49m+}\n\e[0m\\ No newline at end of file\nNo stack found\n").to_stdout
+        expect { stack_master.perform }.to output(/\+  \"a\"\: 1/).to_stdout
+        expect { stack_master.perform }.to output(/\+  \"param1\"\: \"hello\"/).to_stdout
+        expect { stack_master.perform }.to output(/No stack found/).to_stdout
       end
     end
 
     context "stack update" do
-      before do
-        allow(StackMaster::Stack).to receive(:find).with('us-east-1', "myapp_vpc").and_return stack
-        allow(stack).to receive(:template_body).and_return "{}"
-        allow(stack_definition).to receive(:parameters).and_return []
-        allow(stack_definition).to receive(:template_body).and_return "{\"a\": 1}"
-      end
-
       it "outputs the stack diff" do
         expect { stack_master.perform }.to output(/\+  \"a\"\: 1/).to_stdout
+        expect { stack_master.perform }.to output(/\+  \"param1\"\: \"hello\"/).to_stdout
+        expect { stack_master.perform }.to_not output(/No stack found/).to_stdout
       end
     end
   end
