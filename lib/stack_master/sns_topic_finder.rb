90ccab747c40cd1cf0b9badb84c78f6adb6769b5
Glen Stampoultzis <glen.stampoultzis@envato.com>
2015-10-29 10:01:08 +1100

    Add topic finder

---
+++ lib/stack_master/sns_topic_finder.rb
@@ -0,0 +1,27 @@
+module StackMaster
+  class SnsTopicFinder
+    TopicNotFound = Class.new(StandardError)
+
+    def initialize(region)
+      @resource = Aws::SNS::Resource.new(region: region)
+    end
+
+    def find(reference)
+      $stderr.puts "Resolving SNS topic reference '#{reference}'"
+      raise ArgumentError, 'SNS topic references must be non-empty strings' unless reference.is_a?(String) && !reference.empty?
+
+      topic = @resource.topics.detect { |t| topic_name_from_arn(t.arn) == reference }
+
+      raise TopicNotFound, "No topic with name #{reference} found" unless topic
+
+      topic.arn
+    end
+
+    private
+
+    def topic_name_from_arn(arn)
+      arn.split(":")[5]
+    end
+
+  end
+end
