0becbd4c6efae5c9c01389f2dc6e641283adee70
Glen Stampoultzis <glen.stampoultzis@envato.com>
2015-10-29 13:44:46 +1100

    Fix indenting

--- lib/stack_master/stack.rb
+++ lib/stack_master/stack.rb
@@ -42,12 +42,12 @@ module StackMaster
           template_body: template_body)
     end
 
-     def aws_parameters
-       Utils.hash_to_aws_parameters(parameters)
-     end
+    def aws_parameters
+      Utils.hash_to_aws_parameters(parameters)
+    end
 
-     def aws_tags
-       Utils.hash_to_aws_tags(tags)
-     end
+    def aws_tags
+      Utils.hash_to_aws_tags(tags)
+    end
   end
 end
