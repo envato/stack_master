6fbe0ad897aab00a093b180c9e58085163487db0
Steve Hodgkiss <steve@hodgkiss.me>
2015-10-29 16:40:21 +1100

    Namespace the CLI class under StackMaster

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,5 +3,7 @@ require 'stack_master'
 require 'stack_master/testing'
 require 'aruba/in_process'
 
-Aruba.process = Aruba::Processes::InProcess
-Aruba.process.main_class = StackMasterCLI
+Aruba.configure do |config|
+  config.command_launcher = :in_process
+  config.main_class = StackMaster::CLI
+end
