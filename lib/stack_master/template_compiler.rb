2786fa2cae5742beafd900347567f126c6b665f1
Glen Stampoultzis <glen.stampoultzis@envato.com>
2015-11-04 16:35:02 +1100

    Don't compress the template compiler output unless necessary.

--- lib/stack_master/template_compiler.rb
+++ lib/stack_master/template_compiler.rb
@@ -1,12 +1,20 @@
 module StackMaster
   class TemplateCompiler
+
+    MAX_TEMPLATE_SIZE = 51200
+
     def self.compile(template_file_path)
       if template_file_path.ends_with?('.rb')
         SparkleFormation.sparkle_path = File.dirname(template_file_path)
         JSON.pretty_generate(SparkleFormation.compile(template_file_path))
       else
-        # Parse the json and rewrite compressed
-        JSON.dump(JSON.parse(File.read(template_file_path)))
+        template_body = File.read(template_file_path)
+        if template_body.size > MAX_TEMPLATE_SIZE
+          # Parse the json and rewrite compressed
+          JSON.dump(JSON.parse(template_body))
+        else
+          template_body
+        end
       end
     end
   end
