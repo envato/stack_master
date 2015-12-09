4606b224bd5b3c4ef8b7ee4d64a476ba595e9e02
lethalpaga <lethalpaga@gmail.com>
2015-12-09 15:39:44 +1300

    Dynamically require parameter resolvers
    This allows the creation of 'plugins' by creating a new gem
    containing a path like lib/stack_master/parameter_resolvers/<resolver_name>

--- lib/stack_master/parameter_resolver.rb
+++ lib/stack_master/parameter_resolver.rb
@@ -27,19 +27,53 @@ module StackMaster
 
     private
 
+    def require_parameter_resolver(file_name)
+      begin
+        require "stack_master/parameter_resolvers/#{file_name}"
+      rescue LoadError
+        raise ResolverNotFound.new(file_name)
+      end
+    end
+
+    def load_parameter_resolver(class_name)
+      begin
+        # Check if the class name already exists
+        return if resolver_class_const(class_name)
+      rescue NameError
+        # If it doesn't, try to load it
+        require_parameter_resolver(class_name.underscore)
+      end
+    end
+
     def resolve_parameter_value(parameter_value)
       return parameter_value if String === parameter_value || parameter_value.nil?
       raise InvalidParameter, parameter_value unless Hash === parameter_value
       raise InvalidParameter, parameter_value unless parameter_value.keys.size == 1
-      resolver_class_name = parameter_value.keys.first.to_s.camelize
+
+      resolver_name = parameter_value.keys.first.to_s
+      load_parameter_resolver(resolver_name)
+
       value = parameter_value.values.first
-      resolver_class(resolver_class_name).resolve(value)
+      resolver_class_name = resolver_name.camelize
+      call_resolver(resolver_class_name, value)
+    end
+
+    def call_resolver(class_name, value)
+      resolver_class(class_name).resolve(value)
+    end
+
+    def resolver_class_name(class_name)
+      "StackMaster::ParameterResolvers::#{class_name.camelize}"
+    end
+
+    def resolver_class_const(class_name)
+      Kernel.const_get(resolver_class_name(class_name))
     end
 
     def resolver_class(class_name)
       @resolvers.fetch(class_name) do
         begin
-          @resolvers[class_name] = Kernel.const_get("StackMaster::ParameterResolvers::#{class_name}").new(@config, @stack_definition)
+          @resolvers[class_name] = resolver_class_const(class_name).new(@config, @stack_definition)
         rescue NameError
           raise ResolverNotFound, "Could not find parameter resolver called #{class_name}, please double check your configuration"
         end
