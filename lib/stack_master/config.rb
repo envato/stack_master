f723bad3d69706d7c552cbda0bc42f52cbdd83ff
lethalpaga <lethalpaga@gmail.com>
2016-01-19 16:45:41 +1300

    Allow overriding the S3 config in the stack definition

--- lib/stack_master/config.rb
+++ lib/stack_master/config.rb
@@ -78,7 +78,7 @@ module StackMaster
         region = Utils.underscore_to_hyphen(region)
         stacks_for_region.each do |stack_name, attributes|
           stack_name = Utils.underscore_to_hyphen(stack_name)
-          stack_attributes = build_stack_defaults(region).deeper_merge!(attributes).merge(
+          stack_attributes = attributes.deeper_merge(build_stack_defaults(region)).merge(
             'region' => region,
             'stack_name' => stack_name,
             'base_dir' => @base_dir,
