2eea8acb31a4092fdc977c5a320571f09b87bfff
Glen Stampoultzis <glen.stampoultzis@envato.com>
2016-01-29 10:30:30 +1100

    Allow stack master to handle numeric parameters
    Fixes: https://github.com/envato/stack_master/issues/66

--- spec/stack_master/utils_spec.rb
+++ spec/stack_master/utils_spec.rb
@@ -17,13 +17,14 @@ RSpec.describe StackMaster::Utils do
   end
 
   describe ".hash_to_aws_parameters" do
-    let(:params) { { 'param1' => 'value1', 'param2' => 'value2' } }
+    let(:params) { { 'param1' => 'value1', 'param2' => 'value2', 'param3' => 3 } }
     subject(:aws_params) { StackMaster::Utils.hash_to_aws_parameters(params) }
 
-    it "converts to aws parameters" do
+    it "converts to aws parameters (numerics convert to strings)" do
       expect(aws_params).to eq([
         { parameter_key: 'param1', parameter_value: 'value1' },
-        { parameter_key: 'param2', parameter_value: 'value2' }
+        { parameter_key: 'param2', parameter_value: 'value2' },
+        { parameter_key: 'param3', parameter_value: '3' }
       ])
     end
   end
