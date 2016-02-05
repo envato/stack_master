f2255ecdd2aa1139d32fcd229e512765d3bca636
Dave Steinberg <dave@steinbergcomputing.com>
2016-02-05 15:47:02 +0000

    rm ActiveSupport::Concern dependency

--- lib/stack_master/commands/terminal_helper.rb
+++ lib/stack_master/commands/terminal_helper.rb
@@ -1,19 +1,13 @@
-require 'active_support/concern'
-
 module StackMaster
   module Commands
     module TerminalHelper
-      extend ActiveSupport::Concern
+      def window_size
+        size = ENV.fetch("COLUMNS") { `tput cols`.chomp }
 
-      included do
-        def window_size
-          size = ENV.fetch("COLUMNS") { `tput cols`.chomp }
-
-          if size.nil? || size == ""
-            80
-          else
-            size.to_i
-          end
+        if size.nil? || size == ""
+          80
+        else
+          size.to_i
         end
       end
     end
