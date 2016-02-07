745de188adfd723c3fe061cc31408f281caf3ab0
Steve Hodgkiss <steve@hodgkiss.me>
2016-02-07 13:43:49 +1100

    Add prompter spec

---
+++ spec/stack_master/prompter_spec.rb
@@ -0,0 +1,23 @@
+RSpec.describe StackMaster::Prompter do
+  include StackMaster::Prompter
+
+  context 'when STDIN is not a TTY' do
+    before do
+      allow(StackMaster.stdin).to receive(:tty?).and_return(false)
+    end
+
+    it 'defaults to no and outputs info about -f' do
+      expect { ask?('blah') }.to output(/To force yes use -f/).to_stdout
+    end
+  end
+
+  context 'when STDOUT is not a TTY' do
+    before do
+      allow(StackMaster.stdout).to receive(:tty?).and_return(false)
+    end
+
+    it 'defaults to no and outputs info about -f' do
+      expect { ask?('blah') }.to output(/To force yes use -f/).to_stdout
+    end
+  end
+end
