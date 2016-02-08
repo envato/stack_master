d7d84b61f3d902fb199455106f2d3b8c1a9408fd
Steve Hodgkiss <steve@hodgkiss.me>
2016-02-08 19:18:15 +1100

    Update output so to be “stack_name: valid/invalid\n” for each stack

--- lib/stack_master/validator.rb
+++ lib/stack_master/validator.rb
@@ -7,11 +7,12 @@ module StackMaster
     end
 
     def perform
+      StackMaster.stdout.print "#{@stack_definition.stack_name}: "
       template_body = TemplateCompiler.compile(@stack_definition.template_file_path)
       cf.validate_template(template_body: template_body)
-      StackMaster.stdout.puts "Valid"
+      StackMaster.stdout.puts "valid"
     rescue Aws::CloudFormation::Errors::ValidationError => e
-      $stderr.puts "Validation Failed"
+      StackMaster.stdout.puts "invalid"
       $stderr.puts e.message
     end
 
