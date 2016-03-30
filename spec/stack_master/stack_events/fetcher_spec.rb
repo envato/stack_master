00d6410916c0956c217272856a54bfe4d37b3ae9
Steve Hodgkiss <steve@hodgkiss.me>
2016-03-30 14:41:55 +1100

    Extract a class to handle paged responses with next_token

--- spec/stack_master/stack_events/fetcher_spec.rb
+++ spec/stack_master/stack_events/fetcher_spec.rb
@@ -1,43 +1,22 @@
 RSpec.describe StackMaster::StackEvents::Fetcher do
   let(:cf) { Aws::CloudFormation::Client.new }
+  let(:stack_name) { 'blah' }
 
   before do
     allow(Aws::CloudFormation::Client).to receive(:new).and_return(cf)
     allow(StackMaster::StackEvents::Streamer).to receive(:stream)
+    allow(StackMaster::PagedResponseAccumulator).to receive(:call).with(StackMaster.cloud_formation_driver, :describe_stack_events, { stack_name: stack_name }, :stack_events).and_return(OpenStruct.new(stack_events: events))
   end
 
   context 'with 2 stack events' do
     let(:events) { [
-      { event_id: '1', stack_id: '1', stack_name: 'blah', timestamp: Time.now},
-      { event_id: '2', stack_id: '1', stack_name: 'blah', timestamp: Time.now}
+      OpenStruct.new(event_id: '1', stack_id: '1', stack_name: 'blah', timestamp: Time.now),
+      OpenStruct.new(event_id: '2', stack_id: '1', stack_name: 'blah', timestamp: Time.now)
     ] }
-
-    before do
-      cf.stub_responses(:describe_stack_events, stack_events: events)
-    end
 
     it 'returns stack events' do
-      events = StackMaster::StackEvents::Fetcher.fetch('blah', 'us-east-1')
+      events = StackMaster::StackEvents::Fetcher.fetch(stack_name, 'us-east-1')
       expect(events.count).to eq 2
-    end
-  end
-
-  context 'when more than one page is present' do
-    let(:page_one_events) { [
-      { event_id: '1', stack_id: '1', stack_name: 'blah', timestamp: Time.now},
-      { event_id: '2', stack_id: '1', stack_name: 'blah', timestamp: Time.now}
-    ] }
-    let(:page_two_events) { [
-      { event_id: '3', stack_id: '1', stack_name: 'blah', timestamp: Time.now}
-    ] }
-
-    before do
-      cf.stub_responses(:describe_stack_events, { stack_events: page_one_events, next_token: 'blah' }, { stack_events: page_two_events } )
-    end
-
-    it 'returns all the stack events combined' do
-      events = StackMaster::StackEvents::Fetcher.fetch('blah', 'us-east-1')
-      expect(events.count).to eq 3
     end
   end
 
@@ -48,17 +27,13 @@ RSpec.describe StackMaster::StackEvents::Fetcher do
 
     let(:events) {
       [
-        { event_id: '1', stack_id: '1', stack_name: 'blah', timestamp: two_pm },
-        { event_id: '2', stack_id: '1', stack_name: 'blah', timestamp: four_pm },
+        OpenStruct.new(event_id: '1', stack_id: '1', stack_name: 'blah', timestamp: two_pm),
+        OpenStruct.new(event_id: '2', stack_id: '1', stack_name: 'blah', timestamp: four_pm),
       ]
     }
 
-    before do
-      cf.stub_responses(:describe_stack_events, stack_events: events)
-    end
-
     it 'only returns events after the timestamp' do
-      events = StackMaster::StackEvents::Fetcher.fetch('blah', 'us-east-1', from: three_pm)
+      events = StackMaster::StackEvents::Fetcher.fetch(stack_name, 'us-east-1', from: three_pm)
       expect(events.map(&:event_id)).to eq ['2']
     end
   end
