e7298cfa8c00c4f9567e266a1d7addfb80b4f718
Yasith Fernando <thekindofme@gmail.com>
2016-04-01 12:55:54 +1100

    Throw an exception if Psych can't read the template

--- lib/stack_master/template_compilers/yaml.rb
+++ lib/stack_master/template_compilers/yaml.rb
@@ -3,10 +3,17 @@ module StackMaster::TemplateCompilers
 
     def self.compile(template_file_path)
       template_body = File.read(template_file_path)
-      JSON.dump(YAML.load(template_body))
+
+      begin
+        JSON.dump(YAML.load(template_body))
+      rescue Psych::Exception
+        raise StackMaster::TemplateCompilers::Yaml::CompileError
+      end
     end
 
     StackMaster::TemplateCompiler.register(:yaml, self)
     StackMaster::TemplateCompiler.register(:yml, self)
+
+    class StackMaster::TemplateCompilers::Yaml::CompileError < RuntimeError; end
   end
 end
