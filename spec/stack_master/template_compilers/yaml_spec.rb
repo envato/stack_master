3bb2f99e090b270a4aeef28a4a54290380a64af2
Soshi Katsuta <soshi.katsuta@gmail.com>
2016-10-10 14:42:27 +0900

    Avoid converting YAML to JSON
    Signed-off-by: Soshi Katsuta <soshi.katsuta@gmail.com>

--- spec/stack_master/template_compilers/yaml_spec.rb
+++ spec/stack_master/template_compilers/yaml_spec.rb
@@ -7,11 +7,10 @@ RSpec.describe StackMaster::TemplateCompilers::Yaml do
     context 'valid YAML template' do
       let(:template_file_path) { 'spec/fixtures/templates/yml/valid_myapp_vpc.yml' }
 
-      it 'produces valid JSON' do
-        valid_myapp_vpc_as_json = File.read('spec/fixtures/templates/json/valid_myapp_vpc.json')
-        valid_myapp_vpc_as_hash = JSON.parse(valid_myapp_vpc_as_json)
+      it 'produces valid YAML' do
+        valid_myapp_vpc_yaml = File.read('spec/fixtures/templates/yml/valid_myapp_vpc.yml')
 
-        expect(JSON.parse(compile)).to eq(valid_myapp_vpc_as_hash)
+        expect(compile).to eq(valid_myapp_vpc_yaml)
       end
     end
   end
