29235d0d296d59ec7c17c0373b88d5ea1d6fe7e0
Dave Steinberg <dave@steinbergcomputing.com>
2016-12-02 05:05:24 +0000

    support setting ::SparkleFormation.sparkle_path via a compiler option

--- lib/stack_master/template_compilers/sparkle_formation.rb
+++ lib/stack_master/template_compilers/sparkle_formation.rb
@@ -5,8 +5,13 @@ module StackMaster::TemplateCompilers
       require 'stack_master/sparkle_formation/template_file'
     end
 
-    def self.compile(template_file_path, compiler_options={})
-      ::SparkleFormation.sparkle_path = File.dirname(template_file_path)
+    def self.compile(template_file_path, compiler_options = {})
+      if compiler_options["sparkle_path"]
+        ::SparkleFormation.sparkle_path = File.expand_path(compiler_options["sparkle_path"])
+      else
+        ::SparkleFormation.sparkle_path = File.dirname(template_file_path)
+      end
+
       JSON.pretty_generate(::SparkleFormation.compile(template_file_path))
     end
 
