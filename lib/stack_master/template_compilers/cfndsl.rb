1ba8e6bbf611646931531d5d93e939c239cc29cb
mbakerbp <michael.baker@bulletproof.net>
2016-12-09 06:53:07 +0800

    Split lines out

--- lib/stack_master/template_compilers/cfndsl.rb
+++ lib/stack_master/template_compilers/cfndsl.rb
@@ -6,7 +6,8 @@ module StackMaster::TemplateCompilers
 
     def self.compile(config, template_file_path, stack_definition)
       params = []
-      params.push([:yaml, File.join(config.base_dir, 'external_parameters', stack_definition.cfndsl_external_parameters)]) if stack_definition.cfndsl_external_parameters.length > 0
+      params.push([:yaml, File.join(config.base_dir, 'external_parameters', 
+                  stack_definition.cfndsl_external_parameters)]) if stack_definition.cfndsl_external_parameters.length > 0
       ::CfnDsl.eval_file_with_extras(template_file_path, params).to_json
     end
 
