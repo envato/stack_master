d474a48a09d667cc842fd926e348674d221ebd0f
samuel.suarez.sanchez <samuel.suarez.sanchez@envato.com>
2017-01-31 21:09:53 +0100

    Use per-region CF driver to resolve outputs

--- lib/stack_master/parameter_resolvers/stack_output.rb
+++ lib/stack_master/parameter_resolvers/stack_output.rb
@@ -10,6 +10,7 @@ module StackMaster
         @config = config
         @stack_definition = stack_definition
         @stacks = {}
+        @cf_drivers = {}
         @output_regex = %r{(?:(?<region>[^:]+):)?(?<stack_name>[^:/]+)/(?<output_name>.+)}
       end
 
@@ -51,15 +52,23 @@ module StackMaster
         stack_key = stack_key(stack_name, unaliased_region)
 
         @stacks.fetch(stack_key) do
-          original_region = cf.region
-
-          cf.set_region(unaliased_region) if region && original_region != unaliased_region
-
-          cf_stack = cf.describe_stacks(stack_name: stack_name).stacks.first
-
-          cf.set_region(original_region) if region && original_region != unaliased_region
-
+          regional_cf = cf_for_region(region)
+          cf_stack = regional_cf.describe_stacks(stack_name: stack_name).stacks.first
           @stacks[stack_key] = cf_stack
+        end
+      end
+
+      def stack_key(stack_name, region)
+        "#{region}:#{stack_name}"
+      end
+
+      def cf_for_region(region)
+        return cf if cf.region == region
+
+        @cf_drivers.fetch(region) do
+          cloud_formation_driver = cf.class.new
+          cloud_formation_driver.set_region(region)
+          @cf_drivers[region] = cloud_formation_driver
         end
       end
     end
