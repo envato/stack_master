f45d74cc349a6c23c086496e6afc5469f99711c2
Robert Bayerl <robertjbayerl@gmail.com>
2017-04-22 13:16:48 -0500

    Exit with error if stack ends in a failed state

--- spec/stack_master/stack_events/streamer_spec.rb
+++ spec/stack_master/stack_events/streamer_spec.rb
@@ -30,4 +30,18 @@ RSpec.describe StackMaster::StackEvents::Streamer do
     StackMaster::StackEvents::Streamer.stream(stack_name, region, sleep_between_fetches: 0, io: io)
     expect(io.string).to include('UPDATE_COMPLETE')
   end
+
+  context "the stack is in a failed state" do
+    let(:events_second_call) {
+      events_first_call + [
+        OpenStruct.new(event_id: '4', resource_status: 'ROLLBACK_FAILED', resource_type: 'AWS::CloudFormation::Stack', logical_resource_id: stack_name, timestamp: Time.now)
+      ]
+    }
+
+    it 'raises an error on failure' do
+      expect {
+        StackMaster::StackEvents::Streamer.stream(stack_name, region, sleep_between_fetches: 0)
+      }.to raise_error(StackMaster::StackEvents::Streamer::StackFailed)
+    end
+  end
 end
