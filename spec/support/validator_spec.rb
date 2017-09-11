1270547210be337d4f667674ae0d81b7bc1ee023
Patrick Robinson <patrick.robinson@envato.com>
2017-09-11 14:54:19 +1000

    Use positive language for specs

--- spec/support/validator_spec.rb
+++ spec/support/validator_spec.rb
@@ -1,7 +1,7 @@
 RSpec.shared_examples "validate valid parameter" do |validator_class, parameter|
   subject(:valid_parameter_validator) { validator_class.new('name', validator_definition, parameter).tap {|validator| validator.validate} }
 
-  it 'should be valid' do
+  it 'is valid' do
     expect(valid_parameter_validator.is_valid).to be_truthy
   end
 end
@@ -9,11 +9,11 @@ end
 RSpec.shared_examples "validate invalid parameter" do |validator_class, parameter, error|
   subject(:invalid_parameter_validator) { validator_class.new('name', validator_definition, parameter).tap {|validator| validator.validate} }
 
-  it 'should not be valid' do
+  it 'is not valid' do
     expect(invalid_parameter_validator.is_valid).to be_falsey
   end
 
-  it 'should have an error' do
+  it 'has an error' do
     expect(invalid_parameter_validator.error).to eql "name:#{error} does not match allowed_pattern:#{validator_definition[:allowed_pattern]}"
   end
 end
