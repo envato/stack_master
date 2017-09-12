12a449ad9adac2adf4b0bf20c40983512e9de5a6
Grant Tibbey <grant.tibbey@envato.com>
2017-09-12 19:07:59 +1000

    remove unnecessary subject name

--- spec/support/validator_spec.rb
+++ spec/support/validator_spec.rb
@@ -1,23 +1,23 @@
 def validate_valid_parameter(parameter)
   context "with parameter #{parameter}" do
-    subject(:validator) { described_class.new('name', definition, parameter).tap {|validator| validator.validate} }
+    subject { described_class.new('name', definition, parameter).tap { |validator| validator.validate } }
 
     it 'is valid' do
-      expect(validator.is_valid).to be_truthy
+      expect(subject.is_valid).to be_truthy
     end
   end
 end
 
 def validate_invalid_parameter(parameter, errors)
   context "with parameter #{parameter}" do
-    subject(:validator) { described_class.new(name, definition, parameter).tap {|validator| validator.validate} }
+    subject { described_class.new(name, definition, parameter).tap { |validator| validator.validate } }
 
     it 'is not valid' do
-      expect(validator.is_valid).to be_falsey
+      expect(subject.is_valid).to be_falsey
     end
 
     it 'has an error' do
-      expect(validator.error).to eql error_message.call(errors, definition)
+      expect(subject.error).to eql error_message.call(errors, definition)
     end
   end
 end
