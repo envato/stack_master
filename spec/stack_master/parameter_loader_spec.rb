cafb4916ca14d1e2a7507ca315ab6e4e0332d561
Grant Tibbey <grant.tibbey@envato.com>
2017-10-17 12:29:52 +1100

    DRY up a bit more

--- spec/stack_master/parameter_loader_spec.rb
+++ spec/stack_master/parameter_loader_spec.rb
@@ -1,15 +1,17 @@
 RSpec.describe StackMaster::ParameterLoader do
-
   let(:stack_file_name) { '/base_dir/parameters/stack_name.yml' }
   let(:region_file_name) { '/base_dir/parameters/us-east-1/stack_name.yml' }
 
   subject(:parameters) { StackMaster::ParameterLoader.load([stack_file_name, region_file_name]) }
 
+  before do
+    file_mock(stack_file_name, stack_file_returns)
+    file_mock(region_file_name, region_file_returns)
+  end
+
   context 'no parameter file' do
-    before do
-      file_mock(stack_file_name, false)
-      file_mock(region_file_name, false)
-    end
+    let(:stack_file_returns) { {exists: false} }
+    let(:region_file_returns) { {exists: false} }
 
     it 'returns empty parameters' do
       expect(parameters).to eq(template_parameters: {}, compile_time_parameters: {})
@@ -17,10 +19,8 @@ RSpec.describe StackMaster::ParameterLoader do
   end
 
   context 'an empty stack parameter file' do
-    before do
-      file_mock(stack_file_name, true, '')
-      file_mock(region_file_name, false)
-    end
+    let(:stack_file_returns) { {exists: true, read: ''} }
+    let(:region_file_returns) { {exists: false} }
 
     it 'returns an empty hash' do
       expect(parameters).to eq({template_parameters: {}, compile_time_parameters: {}})
@@ -28,10 +28,8 @@ RSpec.describe StackMaster::ParameterLoader do
   end
 
   context 'stack parameter file' do
-    before do
-      file_mock(stack_file_name, true, 'Param1: value1')
-      file_mock(region_file_name, false)
-    end
+    let(:stack_file_returns) { {exists: true, read: 'Param1: value1'} }
+    let(:region_file_returns) { {exists: false} }
 
     it 'returns params from stack_name.yml' do
       expect(parameters).to eq(template_parameters: {'Param1' => 'value1'}, compile_time_parameters: {})
@@ -39,10 +37,8 @@ RSpec.describe StackMaster::ParameterLoader do
   end
 
   context 'region parameter file' do
-    before do
-      file_mock(stack_file_name, false)
-      file_mock(region_file_name, true, 'Param2: value2')
-    end
+    let(:stack_file_returns) { {exists: false } }
+    let(:region_file_returns) { {exists: true, read: 'Param2: value2'} }
 
     it 'returns params from the region base stack_name.yml' do
       expect(parameters).to eq(template_parameters: {'Param2' => 'value2'}, compile_time_parameters: {})
@@ -50,10 +46,8 @@ RSpec.describe StackMaster::ParameterLoader do
   end
 
   context 'stack and region parameter file' do
-    before do
-      file_mock(stack_file_name, true, "Param1: value1\nParam2: valueX")
-      file_mock(region_file_name, true, 'Param2: value2')
-    end
+    let(:stack_file_returns) { {exists: true, read: "Param1: value1\nParam2: valueX" } }
+    let(:region_file_returns) { {exists: true, read: 'Param2: value2'} }
 
     it 'returns params from the region base stack_name.yml' do
       expect(parameters).to eq(template_parameters: {'Param1' => 'value1', 'Param2' => 'value2'}, compile_time_parameters: {})
@@ -63,43 +57,37 @@ RSpec.describe StackMaster::ParameterLoader do
   context 'compile time parameters' do
 
     context 'stack parameter file' do
-      before do
-        file_mock(stack_file_name, true, "Param1: value1\nCompileTimeParameters:\n  Param2: value2")
-        file_mock(region_file_name, false)
-      end
+      let(:stack_file_returns) { {exists: true, read: "Param1: value1\nCompileTimeParameters:\n  Param2: value2" } }
+      let(:region_file_returns) { {exists: false} }
 
       it 'returns params from stack_name.yml' do
-        expect(parameters).to eq(template_parameters:{'Param1' => 'value1'}, compile_time_parameters:{'Param2' => 'value2'})
+        expect(parameters).to eq(template_parameters: {'Param1' => 'value1'}, compile_time_parameters: {'Param2' => 'value2'})
       end
     end
 
     context 'stack and region parameter file' do
-      before do
-        file_mock(stack_file_name, true, "Param1: value1\nCompileTimeParameters:\n  Param2: valueX")
-        file_mock(region_file_name, true, "CompileTimeParameters:\n  Param2: value2")
-      end
+      let(:stack_file_returns) { {exists: true, read: "Param1: value1\nCompileTimeParameters:\n  Param2: valueX" } }
+      let(:region_file_returns) { {exists: true, read: "CompileTimeParameters:\n  Param2: value2"} }
 
       it 'returns params from the region base stack_name.yml' do
-        expect(parameters).to eq(template_parameters: {'Param1' => 'value1'}, compile_time_parameters:{'Param2' => 'value2'})
+        expect(parameters).to eq(template_parameters: {'Param1' => 'value1'}, compile_time_parameters: {'Param2' => 'value2'})
       end
     end
 
   end
 
   context 'underscored parameter names' do
-    before do
-      file_mock(stack_file_name, true, 'vpc_id: vpc-xxxxxx')
-      file_mock(region_file_name, false)
-    end
+    let(:stack_file_returns) { {exists: true, read: 'vpc_id: vpc-xxxxxx' } }
+    let(:region_file_returns) { {exists: false} }
 
     it 'camelcases them' do
       expect(parameters).to eq(template_parameters: {'VpcId' => 'vpc-xxxxxx'}, compile_time_parameters: {})
     end
   end
 
-  def file_mock(file_name, exists_return, read_return = nil)
-    allow(File).to receive(:exists?).with(file_name).and_return(exists_return)
-    allow(File).to receive(:read).with(file_name).and_return(read_return) if read_return
+  def file_mock(file_name, exists: false, read: nil)
+    allow(File).to receive(:exists?).with(file_name).and_return(exists)
+    allow(File).to receive(:read).with(file_name).and_return(read) if read
   end
 
 end
