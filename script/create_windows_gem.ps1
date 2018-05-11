f9589f4cd31cd18dcb9226e587995d75fa617928
Patrick Robinson <patrick.robinson@envato.com>
2018-05-11 22:26:33 +1000

    Buildkite does not have a TTY

--- script/create_windows_gem.ps1
+++ script/create_windows_gem.ps1
@@ -1,2 +1,2 @@
-docker build -q -f Dockerfile.windows.ci -t ruby-slim .
-docker run -ti --rm -v ${PWD}:C:\stack_master -w C:\stack_master ruby-slim gem build stack_master.gemspec
+docker build -f Dockerfile.windows.ci -t ruby-slim .
+docker run -i --rm -v ${PWD}:C:\stack_master -w C:\stack_master ruby-slim gem build stack_master.gemspec
