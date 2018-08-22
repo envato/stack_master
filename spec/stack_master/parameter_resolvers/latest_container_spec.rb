4e84303151050a704fc66b30ceac4447aa7aac58
Patrick Robinson <patrick.robinson@envato.com>
2018-08-22 21:46:54 +1000

    Add pagination test

--- spec/stack_master/parameter_resolvers/latest_container_spec.rb
+++ spec/stack_master/parameter_resolvers/latest_container_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe StackMaster::ParameterResolvers::LatestContainer do
 
   context 'when matches are found' do
     before do
-      ecr.stub_responses(:describe_images, image_details: [
+      ecr.stub_responses(:describe_images, next_token: nil, image_details: [
         { registry_id: '012345678910', image_digest: 'decafc0ffee', image_pushed_at: Time.utc(2015,1,2,0,0), image_tags: ['v1'] },
         { registry_id: '012345678910', image_digest: 'deadbeef', image_pushed_at: Time.utc(2015,1,3,0,0), image_tags: ['v2'] }
       ])
@@ -23,7 +23,7 @@ RSpec.describe StackMaster::ParameterResolvers::LatestContainer do
 
   context 'when there are multiple tags including latest' do
     before do
-      ecr.stub_responses(:describe_images, image_details: [
+      ecr.stub_responses(:describe_images, next_token: nil, image_details: [
         { registry_id: '012345678910', image_digest: 'decafc0ffee', image_pushed_at: Time.utc(2015,1,2,0,0), image_tags: ['v1'] },
         { registry_id: '012345678910', image_digest: 'deadbeef', image_pushed_at: Time.utc(2015,1,3,0,0), image_tags: ['latest', 'v2'] }
       ])
@@ -36,7 +36,7 @@ RSpec.describe StackMaster::ParameterResolvers::LatestContainer do
 
   context 'when no matches are found' do
     before do
-      ecr.stub_responses(:describe_images, image_details: [])
+      ecr.stub_responses(:describe_images, next_token: nil, image_details: [])
     end
 
     it 'returns nil' do
@@ -46,14 +46,33 @@ RSpec.describe StackMaster::ParameterResolvers::LatestContainer do
 
   context 'when registry_id is passed in' do
     before do
-      ecr.stub_responses(:describe_images, image_details: [
+      ecr.stub_responses(:describe_images, next_token: nil, image_details: [
         { registry_id: '012345678910', image_digest: 'decafc0ffee', image_pushed_at: Time.utc(2015,1,2,0,0), image_tags: ['v1'] },
       ])
     end
 
     it 'passes registry_id to describe_images' do
       expect(ecr).to receive(:describe_images).with(repository_name: "foo", registry_id: "012345678910", next_token: nil)
-      resolver.resolve({'repository_name' => 'foo', 'registry_id' => "012345678910"})
+      resolver.resolve({'repository_name' => 'foo', 'registry_id' => '012345678910'})
+    end
+  end
+
+  context 'when there are multiple pages' do
+    before do
+      ecr.stub_responses(:describe_images, [
+        { next_token: '1', image_details: [
+          { registry_id: '012345678910', image_digest: 'decafc0ffee', image_pushed_at: Time.utc(2015,1,2,0,0), image_tags: ['v1'] },
+          { registry_id: '012345678910', image_digest: 'deadbeef', image_pushed_at: Time.utc(2015,1,3,0,0), image_tags: ['latest', 'v2'] }
+        ]},
+        { next_token: nil, image_details: [
+          { registry_id: '012345678910', image_digest: 'decafc0ffee', image_pushed_at: Time.utc(2015,1,4,0,0), image_tags: ['v3'] },
+          { registry_id: '012345678910', image_digest: 'deadbeef', image_pushed_at: Time.utc(2015,1,5,0,0), image_tags: ['v4'] }
+        ]}
+      ])
+    end
+
+    it 'takes all pages into account' do
+      expect(resolver.resolve({'repository_name' => 'foo'})).to eq '012345678910.dkr.ecr.us-east-1.amazonaws.com/foo:v4'
     end
   end
 end
