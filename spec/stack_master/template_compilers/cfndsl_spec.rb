4d2f1f55e214690acfff47dc300e1a916439e8f9
Dave Steinberg <dave@steinbergcomputing.com>
2018-09-13 15:23:57 +0000

    separate the compile-time-param example from the standard one

--- spec/stack_master/template_compilers/cfndsl_spec.rb
+++ spec/stack_master/template_compilers/cfndsl_spec.rb
@@ -18,5 +18,15 @@ RSpec.describe StackMaster::TemplateCompilers::Cfndsl do
         expect(JSON.parse(compile)).to eq(JSON.parse(valid_compiled_json))
       end
     end
+
+    context 'with compile time parameters' do
+      let(:template_file_path) { 'spec/fixtures/templates/rb/cfndsl/sample-ctp.rb' }
+      let(:valid_compiled_json_path) { 'spec/fixtures/templates/rb/cfndsl/sample-ctp.json' }
+
+      it 'produces valid JSON' do
+        valid_compiled_json = File.read(valid_compiled_json_path)
+        expect(JSON.parse(compile)).to eq(JSON.parse(valid_compiled_json))
+      end
+    end
   end
 end
