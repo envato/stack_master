1f32a27553c4b6fa8feaef18fee025932562d37f
Stanisław Pitucha <stan.pitucha@envato.com>
2019-08-15 14:29:36 +1000

    Restore setting extension
    It exposes that we use the file extension for determining the compiler, but
    it's simpler than branching in other places.

--- lib/stack_master/template_compiler.rb
+++ lib/stack_master/template_compiler.rb
@@ -3,11 +3,7 @@ module StackMaster
     TemplateCompilationFailed = Class.new(RuntimeError)
 
     def self.compile(config, stack_definition, compile_time_parameters, compiler_options = {})
-      compiler = if stack_definition.sparkle_pack_template
-        sparkle_template_compiler(config)
-      else
-        template_compiler_for_file(stack_definition.template_file_path, config)
-      end
+      compiler = template_compiler_for_stack(stack_definition, config)
       compiler.require_dependencies
       compiler.compile(stack_definition, compile_time_parameters, compiler_options)
     rescue StandardError => e
@@ -20,18 +16,16 @@ module StackMaster
     end
 
     # private
-    def self.template_compiler_for_file(template_file_path, config)
-      compiler_name = config.template_compilers.fetch(file_ext(template_file_path))
+    def self.template_compiler_for_stack(stack_definition, config)
+      ext = if stack_definition.sparkle_pack_template
+        :rb
+      else
+        file_ext(stack_definition.template_file_path)
+      end
+      compiler_name = config.template_compilers.fetch(ext)
       @compilers.fetch(compiler_name)
     end
-    private_class_method :template_compiler_for_file
-
-    # private
-    def self.sparkle_template_compiler(config)
-      compiler_name = config.template_compilers.fetch(:rb)
-      @compilers.fetch(compiler_name)
-    end
-    private_class_method :sparkle_template_compiler
+    private_class_method :template_compiler_for_stack
 
     def self.file_ext(template_file_path)
       File.extname(template_file_path).gsub('.', '').to_sym
