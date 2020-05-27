4e06de397c2bc225e2f0a28d38da501f12fb5a75
Liam Dawson <liam.dawson@envato.com>
2020-05-27 14:52:01 +1000

    Add template body format identification specs
    Add passing template body format identification specs, in preparation
    for a failing test case.

--- spec/stack_master/template_utils_spec.rb
+++ spec/stack_master/template_utils_spec.rb
@@ -1,4 +1,20 @@
 RSpec.describe StackMaster::TemplateUtils do
+  describe "#identify_template_format" do
+    subject { described_class.identify_template_format(template_body) }
+
+    context "with a json template body" do
+      let(:template_body) { '{"AWSTemplateFormatVersion": "2010-09-09"}' }
+
+      it { is_expected.to eq(:json) }
+    end
+
+    context "with a non-json template body" do
+      let(:template_body) { 'AWSTemplateFormatVersion: 2010-09-09' }
+
+        it { is_expected.to eq(:yaml) }
+    end
+  end
+
   describe "#maybe_compressed_template_body" do
     subject(:maybe_compressed_template_body) do
       described_class.maybe_compressed_template_body(template_body)
