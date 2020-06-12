31ab0c3269ac9d29062d58107ba1404f150f0368
Steve Hodgkiss <steve@hodgkiss.me>
2020-06-12 13:48:16 +0400

    Update error message for missing params when using parameter_files

--- lib/stack_master/parameter_validator.rb
+++ lib/stack_master/parameter_validator.rb
@@ -9,15 +9,14 @@ module StackMaster
 
     def error_message
       return nil unless missing_parameters?
-      message = "Empty/blank parameters detected. Please provide values for these parameters:"
+      message = "Empty/blank parameters detected. Please provide values for these parameters:\n"
       missing_parameters.each do |parameter_name|
-        message << "\n - #{parameter_name}"
+        message << " - #{parameter_name}\n"
       end
-      message << "\nParameters will be read from files matching the following globs:"
-      base_dir = Pathname.new(@stack_definition.base_dir)
-      @stack_definition.parameter_file_globs.each do |glob|
-        parameter_file = Pathname.new(glob).relative_path_from(base_dir)
-        message << "\n - #{parameter_file}"
+      if @stack_definition.parameter_files.empty?
+        message << message_for_parameter_globs
+      else
+        message << message_for_parameter_files
       end
       message
     end
@@ -27,6 +26,24 @@ module StackMaster
     end
 
     private
+
+    def message_for_parameter_files
+      "Parameters are configured to be read from the following files:\n".tap do |message|
+        @stack_definition.parameter_files.each do |parameter_file|
+          message << " - #{parameter_file}\n"
+        end
+      end
+    end
+
+    def message_for_parameter_globs
+      "Parameters will be read from files matching the following globs:\n".tap do |message|
+        base_dir = Pathname.new(@stack_definition.base_dir)
+        @stack_definition.parameter_file_globs.each do |glob|
+          parameter_file = Pathname.new(glob).relative_path_from(base_dir)
+          message << " - #{parameter_file}\n"
+        end
+      end
+    end
 
     def missing_parameters
       @missing_parameters ||=
