cc6caed15ade1246074bf0c489f6763ab73e662e
Ross Simpson <ross.simpson@envato.com>
2020-07-06 23:59:16 +1000

    Improve testing
    - Use an instance_double for config
    - Add a couple more expectations

--- spec/stack_master/commands/nag_spec.rb
+++ spec/stack_master/commands/nag_spec.rb
@@ -2,7 +2,7 @@ RSpec.describe StackMaster::Commands::Nag do
   let(:region) { 'us-east-1' }
   let(:stack_name) { 'myapp-vpc' }
   let(:stack_definition) { StackMaster::StackDefinition.new(base_dir: '/base_dir', region: region, stack_name: stack_name) }
-  let(:config) { double(find_stack: stack_definition) }
+  let(:config) { instance_double(StackMaster::Config, find_stack: stack_definition) }
   let(:parameters) { {} }
   let(:proposed_stack) {
     StackMaster::Stack.new(
@@ -10,8 +10,8 @@ RSpec.describe StackMaster::Commands::Nag do
       template_format: template_format,
       parameters: parameters)
   }
-  let(:tempfile) { double(:tempfile) }
-  let(:path) { double(:path) }
+  let(:tempfile) { double(Tempfile) }
+  let(:path) { double(String) }
 
   before do
     allow(StackMaster::Stack).to receive(:generate).with(stack_definition, config).and_return(proposed_stack)
@@ -26,6 +26,8 @@ RSpec.describe StackMaster::Commands::Nag do
     let(:template_format) { :json }
 
     it 'calls the nag gem' do
+      expect_any_instance_of(File).to receive(:write).once
+      expect_any_instance_of(File).to receive(:flush).once
       expect_any_instance_of(described_class).to receive(:system).once.with('cfn_nag', /.*\.json/)
       run
     end
@@ -36,6 +38,8 @@ RSpec.describe StackMaster::Commands::Nag do
     let(:template_format) { :yaml }
 
     it 'calls the nag gem' do
+      expect_any_instance_of(File).to receive(:write).once
+      expect_any_instance_of(File).to receive(:flush).once
       expect_any_instance_of(described_class).to receive(:system).once.with('cfn_nag', /.*\.yaml/)
       run
     end
