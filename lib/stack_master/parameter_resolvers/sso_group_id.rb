af6e6bdda9546da80a9842e30963a5eb9b453b8f
Andrew Humphrey <andrew.humphrey@envato.com>
2025-07-10 16:00:14 +1200

    Use the same format as stack_output does to specify the region,identity store and group name

--- lib/stack_master/parameter_resolvers/sso_group_id.rb
+++ lib/stack_master/parameter_resolvers/sso_group_id.rb
@@ -6,16 +6,15 @@ module StackMaster
       def initialize(config, stack_definition)
         @config = config
         @stack_definition = stack_definition
-        raise InvalidParameter, "sso_identity_store_id must be set in stack_master.yml when using sso_group_id resolver" unless @config.sso_identity_store_id
       end
 
       def resolve(value)
-        sso_group_id_finder.find(value, @config.sso_identity_store_id)
+        sso_group_id_finder.find(value)
       end
 
     private
       def sso_group_id_finder
-        StackMaster::SsoGroupIdFinder.new(@stack_definition.region)
+        StackMaster::SsoGroupIdFinder.new()
       end
     end
   end
--- lib/stack_master/sso_group_id_finder.rb
+++ lib/stack_master/sso_group_id_finder.rb
@@ -2,23 +2,26 @@ module StackMaster
   class SsoGroupIdFinder
     SsoGroupNotFound = Class.new(StandardError)
 
-    def initialize(region)
-      @client = Aws::IdentityStore::Client.new({ region: region })
-    end
+    def find(reference)
+      output_regex = %r{(?:(?<region>[^:]+):)?(?<identity_store_id>[^:/]+)/(?<group_name>.+)}
 
-    def find(reference, identity_store_id)
-      raise ArgumentError, 'SSO Group Name must be a non-empty string' unless reference.is_a?(String) && !reference.empty?
+      if !reference.is_a?(String) || !(match = output_regex.match(reference))
+          raise ArgumentError, 'Sso group lookup parameter must be in the form of [region:]identity-store-id/group_name'
+      end
+
+      region = match[:region] || StackMaster.cloud_formation_driver.region
+      client = Aws::IdentityStore::Client.new({ region: region })
 
       next_token = nil
       begin
         loop do
-          response = @client.list_groups({
-            identity_store_id: identity_store_id,
+          response = client.list_groups({
+            identity_store_id: match[:identity_store_id],
             next_token: next_token,
             max_results: 50
           })
 
-          matching_group = response.groups.find { |group| group.display_name == reference }
+          matching_group = response.groups.find { |group| group.display_name == match[:group_name] }
           return matching_group.group_id if matching_group
           break unless response.next_token
           next_token = response.next_token
@@ -27,7 +30,7 @@ module StackMaster
           puts "Error calling ListGroups: #{e.message}"
       end
 
-      raise SsoGroupNotFound, "No group with name #{reference} found in identity store #{identity_store_id}"
+      raise SsoGroupNotFound, "No group with name #{match[:group_name]} found in identity store #{match[:identity_store_id]} in #{region}"
     end
   end
 end
