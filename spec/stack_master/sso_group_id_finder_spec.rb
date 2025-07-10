47d2b64abf64bd9092a689ab4dde150f6426d757
Andrew Humphrey <andrew.humphrey@envato.com>
2025-07-10 14:50:25 +1200

    Revert "Another attempt to fix ruby3 incompatibilities"
    This reverts commit 8280eb39ad84f980de33f4bf84f08ec7cb332389.

--- spec/stack_master/sso_group_id_finder_spec.rb
+++ spec/stack_master/sso_group_id_finder_spec.rb
@@ -1,5 +1,3 @@
-require 'ostruct'
-
 RSpec.describe StackMaster::SsoGroupIdFinder do
   let(:region) { 'us-east-1' }
   let(:identity_store_id) { 'd-12345678' }
@@ -9,46 +7,37 @@ RSpec.describe StackMaster::SsoGroupIdFinder do
   let(:aws_client) { instance_double(Aws::IdentityStore::Client) }
 
   subject(:finder) do
-    # Avoid Ruby 3.x keyword arg stubbing issues
-    allow(Aws::IdentityStore::Client).to receive(:new).and_return(aws_client)
+    allow(Aws::IdentityStore::Client).to receive(:new).with(region: region).and_return(aws_client)
     described_class.new(region)
   end
 
   context 'when the group is found on the first page' do
     let(:response) do
-      OpenStruct.new(
-        groups: [OpenStruct.new(display_name: group_name, group_id: group_id)],
-        next_token: nil
-      )
+      double(groups: [double(display_name: group_name, group_id: group_id)], next_token: nil)
     end
 
-    it 'returns the group ID immediately' do
+    it 'returns the group ID' do
       expect(aws_client).to receive(:list_groups).with(
         identity_store_id: identity_store_id,
         next_token: nil,
         max_results: 50
       ).and_return(response)
 
-      expect(finder.find(group_name, identity_store_id)).to eq(group_id)
+      result = finder.find(group_name, identity_store_id)
+      expect(result).to eq(group_id)
     end
   end
 
   context 'when the group is found on the second page' do
     let(:page_1) do
-      OpenStruct.new(
-        groups: [OpenStruct.new(display_name: 'SomeOtherGroup', group_id: 'wrong-id')],
-        next_token: 'next-token'
-      )
+      double(groups: [double(display_name: 'OtherGroup', group_id: 'zzz')], next_token: 'page-2')
     end
 
     let(:page_2) do
-      OpenStruct.new(
-        groups: [OpenStruct.new(display_name: group_name, group_id: group_id)],
-        next_token: nil
-      )
+      double(groups: [double(display_name: group_name, group_id: group_id)], next_token: nil)
     end
 
-    it 'finds the group after paging' do
+    it 'returns the group ID after paging' do
       expect(aws_client).to receive(:list_groups).with(
         identity_store_id: identity_store_id,
         next_token: nil,
@@ -57,27 +46,22 @@ RSpec.describe StackMaster::SsoGroupIdFinder do
 
       expect(aws_client).to receive(:list_groups).with(
         identity_store_id: identity_store_id,
-        next_token: 'next-token',
+        next_token: 'page-2',
         max_results: 50
       ).and_return(page_2)
 
-      expect(finder.find(group_name, identity_store_id)).to eq(group_id)
+      result = finder.find(group_name, identity_store_id)
+      expect(result).to eq(group_id)
     end
   end
 
-  context 'when the group is not found after all pages' do
+  context 'when the group is not found after paging' do
     let(:page_1) do
-      OpenStruct.new(
-        groups: [OpenStruct.new(display_name: 'Wrong1', group_id: 'id1')],
-        next_token: 'token-2'
-      )
+      double(groups: [double(display_name: 'WrongGroup', group_id: 'aaa')], next_token: 'page-2')
     end
 
     let(:page_2) do
-      OpenStruct.new(
-        groups: [OpenStruct.new(display_name: 'Wrong2', group_id: 'id2')],
-        next_token: nil
-      )
+      double(groups: [double(display_name: 'AnotherWrongGroup', group_id: 'bbb')], next_token: nil)
     end
 
     it 'raises SsoGroupNotFound' do
@@ -89,7 +73,7 @@ RSpec.describe StackMaster::SsoGroupIdFinder do
 
       expect(aws_client).to receive(:list_groups).with(
         identity_store_id: identity_store_id,
-        next_token: 'token-2',
+        next_token: 'page-2',
         max_results: 50
       ).and_return(page_2)
 
@@ -99,32 +83,35 @@ RSpec.describe StackMaster::SsoGroupIdFinder do
     end
   end
 
-  context 'when group name is invalid' do
+  context 'when the reference is invalid' do
     it 'raises ArgumentError for nil' do
       expect {
         finder.find(nil, identity_store_id)
-      }.to raise_error(ArgumentError, /must be a non-empty string/)
+      }.to raise_error(ArgumentError, /SSO Group Name must be a non-empty string/)
     end
 
     it 'raises ArgumentError for empty string' do
       expect {
         finder.find('', identity_store_id)
-      }.to raise_error(ArgumentError, /must be a non-empty string/)
+      }.to raise_error(ArgumentError, /SSO Group Name must be a non-empty string/)
+    end
+
+    it 'raises ArgumentError for non-string type' do
+      expect {
+        finder.find(12345, identity_store_id)
+      }.to raise_error(ArgumentError, /SSO Group Name must be a non-empty string/)
     end
   end
 
   context 'when AWS raises a service error' do
-    it 'prints an error and raises SsoGroupNotFound' do
-      error = Aws::IdentityStore::Errors::ServiceError.new(
-        Seahorse::Client::RequestContext.new,
-        'Simulated AWS error'
-      )
+    let(:aws_error) { Aws::IdentityStore::Errors::ServiceError.new(nil, "AWS error") }
 
-      allow(aws_client).to receive(:list_groups).and_raise(error)
+    it 'raises SsoGroupNotFound' do
+      allow(aws_client).to receive(:list_groups).and_raise(aws_error)
 
       expect {
         finder.find(group_name, identity_store_id)
-      }.to raise_error(StackMaster::SsoGroupIdFinder::SsoGroupNotFound)
+      }.to raise_error(StackMaster::SsoGroupIdFinder::SsoGroupNotFound, /No group with name #{group_name} found/)
     end
   end
 end
