2f31feca67b70518a7d0163894ff1a5bb563e115
Orien Madgwick <vcs@orien.io>
2026-02-18 09:41:39 +1100

    RuboCop: resolve Lint/IneffectiveAccessModifier

--- lib/stack_master/parameter_loader.rb
+++ lib/stack_master/parameter_loader.rb
@@ -1,46 +1,48 @@
 require 'active_support/core_ext/object/deep_dup'
 
 module StackMaster
-  class ParameterLoader
+  module ParameterLoader
     COMPILE_TIME_PARAMETERS_KEY = 'compile_time_parameters'
 
-    def self.load(parameter_files: [], parameters: {})
-      StackMaster.debug 'Searching for parameter files...'
-      all_parameters = parameter_files.map { |file_name| load_parameters(file_name) } + [parameters]
-      all_parameters.each_with_object({ template_parameters: {}, compile_time_parameters: {} }) do |parameters, hash|
-        template_parameters = create_template_parameters(parameters)
-        compile_time_parameters = create_compile_time_parameters(parameters)
+    class << self
+      def load(parameter_files: [], parameters: {})
+        StackMaster.debug 'Searching for parameter files...'
+        all_parameters = parameter_files.map { |file_name| load_parameters(file_name) } + [parameters]
+        all_parameters.each_with_object({ template_parameters: {}, compile_time_parameters: {} }) do |parameters, hash|
+          template_parameters = create_template_parameters(parameters)
+          compile_time_parameters = create_compile_time_parameters(parameters)
 
-        merge_and_camelize(hash[:template_parameters], template_parameters)
-        merge_and_camelize(hash[:compile_time_parameters], compile_time_parameters)
+          merge_and_camelize(hash[:template_parameters], template_parameters)
+          merge_and_camelize(hash[:compile_time_parameters], compile_time_parameters)
+        end
       end
-    end
 
-    private
+      private
 
-    def self.load_parameters(file_name)
-      file_exists = File.exist?(file_name)
-      StackMaster.debug file_exists ? "  #{file_name} found" : "  #{file_name} not found"
-      file_exists ? load_file(file_name) : {}
-    end
-
-    def self.load_file(file_name)
-      YAML.load(File.read(file_name)) || {}
-    end
-
-    def self.create_template_parameters(parameters)
-      parameters.deep_dup.tap do |parameters_clone|
-        parameters_clone.delete(COMPILE_TIME_PARAMETERS_KEY) ||
-          parameters_clone.delete(COMPILE_TIME_PARAMETERS_KEY.camelize)
+      def load_parameters(file_name)
+        file_exists = File.exist?(file_name)
+        StackMaster.debug file_exists ? "  #{file_name} found" : "  #{file_name} not found"
+        file_exists ? load_file(file_name) : {}
       end
-    end
 
-    def self.create_compile_time_parameters(parameters)
-      (parameters[COMPILE_TIME_PARAMETERS_KEY] || parameters[COMPILE_TIME_PARAMETERS_KEY.camelize] || {}).deep_dup
-    end
+      def load_file(file_name)
+        YAML.load(File.read(file_name)) || {}
+      end
 
-    def self.merge_and_camelize(hash, parameters)
-      parameters.each { |key, value| hash[key.camelize] = value }
+      def create_template_parameters(parameters)
+        parameters.deep_dup.tap do |parameters_clone|
+          parameters_clone.delete(COMPILE_TIME_PARAMETERS_KEY) ||
+            parameters_clone.delete(COMPILE_TIME_PARAMETERS_KEY.camelize)
+        end
+      end
+
+      def create_compile_time_parameters(parameters)
+        (parameters[COMPILE_TIME_PARAMETERS_KEY] || parameters[COMPILE_TIME_PARAMETERS_KEY.camelize] || {}).deep_dup
+      end
+
+      def merge_and_camelize(hash, parameters)
+        parameters.each { |key, value| hash[key.camelize] = value }
+      end
     end
   end
 end
--- lib/stack_master/template_compilers/sparkle_formation.rb
+++ lib/stack_master/template_compilers/sparkle_formation.rb
@@ -3,80 +3,82 @@ require 'stack_master/sparkle_formation/compile_time/definitions_validator'
 require 'stack_master/sparkle_formation/compile_time/state_builder'
 
 module StackMaster::TemplateCompilers
-  class SparkleFormation
+  module SparkleFormation
     CompileTime = StackMaster::SparkleFormation::CompileTime
 
-    def self.require_dependencies
-      require 'sparkle_formation'
-      require 'stack_master/sparkle_formation/template_file'
-    end
-
-    def self.compile(template_dir, template, compile_time_parameters, compiler_options = {})
-      sparkle_template = compile_sparkle_template(template_dir, template, compiler_options)
-      definitions = sparkle_template.parameters
-      validate_definitions(definitions)
-      validate_parameters(definitions, compile_time_parameters)
-
-      sparkle_template.compile_time_parameter_setter do
-        sparkle_template.compile_state = create_state(definitions, compile_time_parameters)
+    class << self
+      def require_dependencies
+        require 'sparkle_formation'
+        require 'stack_master/sparkle_formation/template_file'
       end
 
-      JSON.pretty_generate(sparkle_template.dump)
-    end
+      def compile(template_dir, template, compile_time_parameters, compiler_options = {})
+        sparkle_template = compile_sparkle_template(template_dir, template, compiler_options)
+        definitions = sparkle_template.parameters
+        validate_definitions(definitions)
+        validate_parameters(definitions, compile_time_parameters)
 
-    private
+        sparkle_template.compile_time_parameter_setter do
+          sparkle_template.compile_state = create_state(definitions, compile_time_parameters)
+        end
 
-    def self.compile_sparkle_template(template_dir, template, compiler_options)
-      sparkle_path =
-        if compiler_options['sparkle_path']
-          File.expand_path(compiler_options['sparkle_path'])
+        JSON.pretty_generate(sparkle_template.dump)
+      end
+
+      private
+
+      def compile_sparkle_template(template_dir, template, compiler_options)
+        sparkle_path =
+          if compiler_options['sparkle_path']
+            File.expand_path(compiler_options['sparkle_path'])
+          else
+            template_dir
+          end
+
+        collection = ::SparkleFormation::SparkleCollection.new
+        root_pack = ::SparkleFormation::Sparkle.new(
+          root: sparkle_path
+        )
+        collection.set_root(root_pack)
+        if compiler_options['sparkle_packs']
+          compiler_options['sparkle_packs'].each do |pack_name|
+            require pack_name
+            pack = ::SparkleFormation::SparklePack.new(name: pack_name)
+            collection.add_sparkle(pack)
+          end
+        end
+
+        if compiler_options['sparkle_pack_template']
+          unless collection.templates['aws'].include? template
+            raise ArgumentError.new("Template #{template.inspect} not found in any sparkle pack")
+          end
+
+          template_file_path = collection.templates['aws'][template].top['path']
         else
-          template_dir
+          template_file_path = File.join(template_dir, template)
         end
 
-      collection = ::SparkleFormation::SparkleCollection.new
-      root_pack = ::SparkleFormation::Sparkle.new(
-        root: sparkle_path
-      )
-      collection.set_root(root_pack)
-      if compiler_options['sparkle_packs']
-        compiler_options['sparkle_packs'].each do |pack_name|
-          require pack_name
-          pack = ::SparkleFormation::SparklePack.new(name: pack_name)
-          collection.add_sparkle(pack)
-        end
+        sparkle_template = compile_template_with_sparkle_path(template_file_path, sparkle_path)
+        sparkle_template.sparkle.apply(collection)
+        sparkle_template
       end
 
-      if compiler_options['sparkle_pack_template']
-        unless collection.templates['aws'].include? template
-          raise ArgumentError.new("Template #{template.inspect} not found in any sparkle pack")
-        end
-
-        template_file_path = collection.templates['aws'][template].top['path']
-      else
-        template_file_path = File.join(template_dir, template)
+      def compile_template_with_sparkle_path(template_path, sparkle_path)
+        ::SparkleFormation.sparkle_path = sparkle_path
+        ::SparkleFormation.compile(template_path, :sparkle)
       end
 
-      sparkle_template = compile_template_with_sparkle_path(template_file_path, sparkle_path)
-      sparkle_template.sparkle.apply(collection)
-      sparkle_template
-    end
+      def validate_definitions(definitions)
+        CompileTime::DefinitionsValidator.new(definitions).validate
+      end
 
-    def self.compile_template_with_sparkle_path(template_path, sparkle_path)
-      ::SparkleFormation.sparkle_path = sparkle_path
-      ::SparkleFormation.compile(template_path, :sparkle)
-    end
+      def validate_parameters(definitions, compile_time_parameters)
+        CompileTime::ParametersValidator.new(definitions, compile_time_parameters).validate
+      end
 
-    def self.validate_definitions(definitions)
-      CompileTime::DefinitionsValidator.new(definitions).validate
-    end
-
-    def self.validate_parameters(definitions, compile_time_parameters)
-      CompileTime::ParametersValidator.new(definitions, compile_time_parameters).validate
-    end
-
-    def self.create_state(definitions, compile_time_parameters)
-      CompileTime::StateBuilder.new(definitions, compile_time_parameters).build
+      def create_state(definitions, compile_time_parameters)
+        CompileTime::StateBuilder.new(definitions, compile_time_parameters).build
+      end
     end
 
     StackMaster::TemplateCompiler.register(:sparkle_formation, self)
